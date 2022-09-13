fbd99fe6e084a9ad75a62eef4e71f1ac4a6c0bb7
dr-data <data@connect.hku.hk>
2022-09-13 17:58:11 +0800

    fix ipfs

--- public/config.js
+++ public/config.js
@@ -1,17 +1,9 @@
 // Change this file as you need it
 
-const projectId = '1zoY8fVddS0ZCzGf8xMiCYjqVxh'
-const projectSecret = '346542f39547fad839e255ba6e4c1e93'
-
-const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
-
 
 const config = {
 	port: 5001,
 	gateway: 8080,
-	headers: {
-		authorization: auth
-	  }
 }
 
 const node = {
